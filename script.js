a572e5b0cd9e6b9701d6735c798552feb4a71b58
Valentin <56909639+ValentinChaillat@users.noreply.github.com>
2020-01-16 10:28:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function init(){
-
-}
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+    
+  }
